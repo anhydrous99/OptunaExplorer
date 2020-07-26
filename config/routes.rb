e7db166d52b7da7bd780fb9f7e0443bfa9c7d4f2
Armando Herrera <anhydrous99@gmail.com>
2020-07-26 09:54:48 -0500

    upgraded ruby version and got working in wsl

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get '/' => 'sessions#index'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
