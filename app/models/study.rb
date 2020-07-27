492506438454599bdd7b02090d6d18d79f675032
Armando Herrera <anhydrous99@gmail.com>
2020-07-27 18:59:33 -0500

    Added model test and model validations

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,2 +1,8 @@
 class Study < ApplicationRecord
+  validates :study_name, :direction, presence: true
+  validates :direction, inclusion: { in: %w(MAXIMIZE MINIMIZE), message: "%{value} is not a valid input" }
+
+  def get_study_id_from_name(name)
+    Study.select(:study_id).where(name: name).LIMIT(1)
+  end
 end
