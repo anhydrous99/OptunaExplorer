af3906f7163848c48d0e1bfe40fe088dc484fb0b
Armando Herrera <anhydrous99@gmail.com>
2020-07-27 17:48:20 -0500

    changes

--- db/migrate/20200727155321_create_studies.rb
+++ db/migrate/20200727155321_create_studies.rb
@@ -2,8 +2,8 @@ class CreateStudies < ActiveRecord::Migration[6.0]
   def change
     create_table :studies, {:id => false} do |t|
       t.primary_key :study_id
-      t.string :study_name
-      t.string :direction
+      t.string :study_name, null: false, unique: true
+      t.string :direction, null: false
     end
   end
 end
