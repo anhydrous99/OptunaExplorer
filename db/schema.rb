84ba257e8c02b169b8d3a706542de1b2ee486fb1
Armando Herrera <anhydrous99@gmail.com>
2020-07-30 10:08:09 -0500

    Added user views (scoped) instead of devise ones as well as added the username field to the model for use in apartment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_132349) do
+ActiveRecord::Schema.define(version: 2020_07_30_143831) do
 
   create_table "studies", primary_key: "study_id", force: :cascade do |t|
     t.string "study_name", null: false
@@ -71,8 +71,10 @@ ActiveRecord::Schema.define(version: 2020_07_30_132349) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
