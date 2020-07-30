05335c41ac20af86ce3a8b3cd360efb01383a30b
Armando Herrera <anhydrous99@gmail.com>
2020-07-30 18:45:01 -0500

    Removed un-needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
     get :set_failed, on: :member
   end
   resources :studies
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
 
   root to: 'home#index'
 end
