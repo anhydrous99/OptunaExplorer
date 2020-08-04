0634f887653d053ecdf45c8f98ba0eefdd3b6071
Armando Herrera <anhydrous99@gmail.com>
2020-08-04 09:55:57 -0700

    Updated production action_mailer to use Google's smtp through an app password.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "optuna_explorer_production"
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'support@optunaexplorer.com'}
-  config.action_mailer.default_url_options = { :host => "www.optunaexplorer.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'optunaexplorer.com',
+      user_name: 'armando.herrera',
+      password: 'ixfjkmpplqubafum',
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
