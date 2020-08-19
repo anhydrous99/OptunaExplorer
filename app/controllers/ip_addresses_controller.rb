dde9b4ac166dc8c81d087f2bdf5a90265700418c
Armando Herrera <anhydrous99@gmail.com>
2020-08-19 18:56:27 -0500

    Added ufw control

--- app/controllers/ip_addresses_controller.rb
+++ app/controllers/ip_addresses_controller.rb
@@ -12,20 +12,31 @@ class IpAddressesController < ApplicationController
   def create
     @ip_address = IpAddress.new(ipaddress: IPAddr.new(ip_address_params[:ipaddress]).to_i, user_id: current_user.id)
 
-    if @ip_address.save
+    terminal_output = `sudo ufw allow from #{ip_address_params[:ipaddress]} to any port 3306`
+    success = terminal_output.include? 'Rule added'
+
+    if @ip_address.save and success
       redirect_to ip_addresses_path, notice: 'IP Address was successfully created.'
     else
-      render ip_address_path
+      redirect_to ip_addresses_path, alert: 'Unable to add IP Address.'
     end
   end
 
   # DELETE /ip_addresses/1
   def destroy
-    @ip_address.destroy
-    redirect_to ip_addresses_url, notice: 'IP Address was successfully destroyed.'
+    terminal_output = `sudo ufw delete allow from #{IPAddr.new(@ip_address[:ipaddress], Socket::AF_INET).to_s} to any port 3306`
+    success = terminal_output.include? 'Rule deleted'
+
+    if success
+      @ip_address.destroy
+      redirect_to ip_addresses_url, notice: 'IP Address was successfully destroyed.'
+    else
+      redirect_to ip_addresses_url, alert: 'Unable to destroy IP Address.'
+    end
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_ip_address
     @ip_address = IpAddress.find(params[:id])
